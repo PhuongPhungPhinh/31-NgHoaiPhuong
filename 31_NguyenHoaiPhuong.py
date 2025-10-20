322d5950a6981bf4a895cc0c8faf5d7a3c890deb
PhuongPhungPhinh <nguyenphuong27062005@gmail.com>
2025-10-20 22:02:18 +0700

    feat: Implement add student function

---
+++ 31_NguyenHoaiPhuong.py
@@ -0,0 +1,38 @@
+# student_manager.py
+
+student_list = []
+
+def add_student(name, year_of_birth, address):
+    ### VIẾT CODE CỦA BẠN VÀO ĐÂY ###
+    student = {
+        "name": name,
+        "year_of_birth": year_of_birth,
+        "address": address
+    }
+    student_list.append(student)
+    print(f"Da them sinh vien {name} thanh cong.")
+
+def print_student_list():
+    ### VIẾT CODE CỦA BẠN VÀO ĐÂY ###
+    pass
+
+def search_student(search_name):
+    ### VIẾT CODE CỦA BẠN VÀO ĐÂY ###
+    pass
+
+if __name__ == "__main__":
+    print("--- CHUONG TRINH QUAN LY SINH VIEN ---")
+    
+    print("\n1. Them sinh vien:")
+    add_student("Nguyen Van An", 2003, "Da Nang")
+    add_student("Tran Thi Binh", 2002, "Quang Nam")
+    add_student("Le Van Hung", 2003, "Hue")
+
+    print("\n2. In danh sach sinh vien:")
+    print_student_list()
+
+    print("\n3. Tim kiem sinh vien theo ten 'an':")
+    search_student("an")
+    
+    print("\nTim kiem sinh vien theo ten 'Dung':")
+    search_student("Dung")
